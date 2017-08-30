838562b7c021a056b65d36749429eedd2d3b2628
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-08-30 13:02:18 +0900

    Add_userindex_jScroll_autoscroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery.jscroll.min
+//= require test-jscroll
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
