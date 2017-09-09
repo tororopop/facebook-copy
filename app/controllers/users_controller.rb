ad96c0d9f250016ae8c125b696659fb8c12a827c
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-09 17:21:41 +0900

    fixed user index rm current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.search(params[:search]).page(params[:page]).per(18)
+    @userselect = User.search(params[:search])
+    @users = @userselect.where.not(id: current_user.id).page(params[:page]).per(18)
   end
 
   def show
