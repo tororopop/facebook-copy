dd487fbb257305b578c260c2581d4e713ce8dfce
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-09 14:44:08 +0900

    fixed topic user design jscroll kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.search(params[:search]).page(params[:page]).per(6)
+    @users = User.search(params[:search]).page(params[:page]).per(18)
   end
 
   def show
     @user = User.find(params[:id])
-    @topics = @user.topics.page(params[:page]).per(20)
+    @topics = @user.topics.page(params[:page]).per(10)
     @likes = Like.where(topic_id: params[:topic_id])
   end
 
