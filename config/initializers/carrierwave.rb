887678fc16064b2f77faf9fcf70365f9462b5030
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-10 15:55:25 +0900

    fix_exif_rotation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,3 +19,15 @@ CarrierWave.configure do |config|
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/facebook-copy-development'
   end
 end
+
+module CarrierWave
+  module MiniMagick
+    def fix_exif_rotation
+      manipulate! do |img|
+        img.auto_orient
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
