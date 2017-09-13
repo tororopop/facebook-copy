1e5c7d3d1ea9cc2a3e13d96d6ff1239dff86cd42
m.murai <murai@mmurai-no-MacBook-Pro.local>
2017-09-13 16:24:52 +0900

    fixed conversations index views

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
 
   def index
     @users = User.all
-    @conversations = Conversation.all
+    @conversations = Conversation.where("sender_id = ? OR recipient_id = ?",current_user.id , current_user.id)
   end
 
   def create
